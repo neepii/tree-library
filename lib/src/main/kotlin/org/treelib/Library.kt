f70fae52c6800474478cd57bf3c71e9b8c9e4ca4
samodurov.nikita <nikita98351@gmail.com>
2025-03-20 09:21:36 +0300

    feat: add node

--- lib/src/main/kotlin/org/treelib/Library.kt
+++ lib/src/main/kotlin/org/treelib/Library.kt
@@ -1,3 +1,6 @@
 package org.treelib
 
-
+abstract class Node<K: Comparable<K>, V: Any>(var key: K, var data: V) {
+    internal var left: Node<K, V>? = null
+    internal var right: Node<K, V>? = null
+}
